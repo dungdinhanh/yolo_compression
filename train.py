b5f8163b0f60e8dae166ad136c1966f6d2c1949d
Dinh Anh Dung <dinhanhdung1996@gmail.com>
2021-06-19 02:49:39 +0800

    save change

--- train.py
+++ train.py
@@ -643,24 +643,43 @@ if __name__ == '__main__':
     os.makedirs(tensorboard_folder, exist_ok=True)
 
     # Hyperparameters
-    hyper = {'giou': 3.54,  # giou loss gain
-           'cls': 37.4,  # cls loss gain
-           'cls_pw': 1.0,  # cls BCELoss positive_weight
-           'obj': 64.3,  # obj loss gain (*=img_size/320 if img_size != 320)
-           'obj_pw': 1.0,  # obj BCELoss positive_weight
-           'iou_t': 0.20,  # iou training threshold
-           'lr0': 0.01,  # initial learning rate (SGD=5E-3, Adam=5E-4)
-           'lrf': 0.0005,  # final learning rate (with cos scheduler)
-           'momentum': 0.937,  # SGD momentum
-           'weight_decay': 0.0005,  # optimizer weight decay
-           'fl_gamma': 0.0,  # focal loss gamma (efficientDet default is gamma=1.5)
-           'hsv_h': 0.0138,  # image HSV-Hue augmentation (fraction)
-           'hsv_s': 0.678,  # image HSV-Saturation augmentation (fraction)
-           'hsv_v': 0.36,  # image HSV-Value augmentation (fraction)
-           'degrees': 1.98 * 0,  # image rotation (+/- deg)
-           'translate': 0.05 * 0,  # image translation (+/- fraction)
-           'scale': 0.05 * 0,  # image scale (+/- gain)
-           'shear': 0.641 * 0}  # image shear (+/- deg)
+    # hyper = {'giou': 3.54,  # giou loss gain
+    #        'cls': 37.4,  # cls loss gain
+    #        'cls_pw': 1.0,  # cls BCELoss positive_weight
+    #        'obj': 64.3,  # obj loss gain (*=img_size/320 if img_size != 320)
+    #        'obj_pw': 1.0,  # obj BCELoss positive_weight
+    #        'iou_t': 0.20,  # iou training threshold
+    #        'lr0': 0.01,  # initial learning rate (SGD=5E-3, Adam=5E-4)
+    #        'lrf': 0.0005,  # final learning rate (with cos scheduler)
+    #        'momentum': 0.937,  # SGD momentum
+    #        'weight_decay': 0.0005,  # optimizer weight decay
+    #        'fl_gamma': 0.0,  # focal loss gamma (efficientDet default is gamma=1.5)
+    #        'hsv_h': 0.0138,  # image HSV-Hue augmentation (fraction)
+    #        'hsv_s': 0.678,  # image HSV-Saturation augmentation (fraction)
+    #        'hsv_v': 0.36,  # image HSV-Value augmentation (fraction)
+    #        'degrees': 1.98 * 0,  # image rotation (+/- deg)
+    #        'translate': 0.05 * 0,  # image translation (+/- fraction)
+    #        'scale': 0.05 * 0,  # image scale (+/- gain)
+    #        'shear': 0.641 * 0}  # image shear (+/- deg)
+
+    hyper = {'giou': 0.0296,  # giou loss gain
+             'cls': 0.243,  # cls loss gain
+             'cls_pw': 0.631,  # cls BCELoss positive_weight
+             'obj': 0.301,  # obj loss gain (*=img_size/320 if img_size != 320)
+             'obj_pw': 0.911,  # obj BCELoss positive_weight
+             'iou_t': 0.20,  # iou training threshold
+             'lr0': 0.0032,  # initial learning rate (SGD=5E-3, Adam=5E-4)
+             'lrf': 0.12,  # final learning rate (with cos scheduler)
+             'momentum': 0.843,  # SGD momentum
+             'weight_decay': 0.00036,  # optimizer weight decay
+             'fl_gamma': 0.0,  # focal loss gamma (efficientDet default is gamma=1.5)
+             'hsv_h': 0.0138,  # image HSV-Hue augmentation (fraction)
+             'hsv_s': 0.664,  # image HSV-Saturation augmentation (fraction)
+             'hsv_v': 0.464,  # image HSV-Value augmentation (fraction)
+             'degrees': 0.373,  # image rotation (+/- deg)
+             'translate': 0.245,  # image translation (+/- fraction)
+             'scale': 0.898,  # image scale (+/- gain)
+             'shear': 0.602}  # image shear (+/- deg)
 
     # Overwrite hyp with hyp*.txt (optional)
     f = glob.glob('hyp*.txt')
